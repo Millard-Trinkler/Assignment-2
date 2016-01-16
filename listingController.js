72d6cd6edbd569da6e6aae59897fb25be19b45cd
Jacob Millard <millard.jake@ufl.edu>
2016-01-15 20:59:22 -0500

    initial commit

--- listingController.js
+++ listingController.js
@@ -7,8 +7,24 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      var newListing = {
+        code: $scope.code,
+        name: $scope.name,
+        address: $scope.address,
+        coordinates: {
+          latitude: $scope.latitude,
+          longitude: $scope.longitude,
+        }
+      }
+      $scope.listings.push(newListing);
+    };
+    $scope.deleteListing = function(obj) {
+      var index = $scope.listings.indexOf(obj);
+      $scope.listings.splice(index,1);
+    };
+    $scope.showDetails = function(obj) {
+      $scope.detailedInfo = obj;
+    };
   }
 ]);
